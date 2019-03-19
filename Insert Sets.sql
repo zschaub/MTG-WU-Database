9934b2aea60abd3780ef76b2d46e981d2f07a03c
zschaub <bdzschau@gmail.com>
2019-03-19 19:11:10 -0400

    Insert Cards and Sets
    Populate Tables with Cards and Sets

---
+++ Insert Sets.sql
@@ -0,0 +1,6 @@
+INSERT INTO t_set VALUES ('Ixalan', DATE '2017-09-29', '289', 'XLN');
+INSERT INTO t_set VALUES ('Rivals of Ixalan', DATE '2018-01-19', '205', 'RIX');
+INSERT INTO t_set VALUES ('Dominaria', DATE '2018-04-27', '280', 'DOM');
+INSERT INTO t_set VALUES ('Core Set 2019', DATE '2018-07-13', '314', 'M19');
+INSERT INTO t_set VALUES ('Guilds of Ravnica', DATE '2018-10-05', '273', 'GRN');
+INSERT INTO t_set VALUES ('Ravnica Allegiance', DATE '2019-07-25', '273', 'RNA');
